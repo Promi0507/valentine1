4e8487229d8c20aec3bf1f2683d64652447852f2
promi rahman <rahmanpromi9@gmail.com>
2025-02-06 17:08:10 +0000

    intex.html

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const messages =[
+    "Are You Sure?",
+    "Really sure?",
+    "Are You Positive?",
+    "Pookie Please...",
+    "Just Think About It!",
+    "If You Say No, I Will Be Really Sad...",
+    "I Will be very sad...",
+    "It Will be very very sad...",
+    "Ok fine I will stop asking..."
+    "Just kidding, say yes please!  "
+
+];
+
+let messageIndex = 0;
+
+
